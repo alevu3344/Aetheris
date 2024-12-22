f71e25459a3ed8b5c4d14e8599d9f968c9b2a330
alevu3344 <alessandro.valmori2@studio.unibo.it>
2024-12-22 12:10:16 +0100

    initial setup of trending games panel in home

--- php/template/home_content.php
+++ php/template/home_content.php
@@ -1,2 +1,16 @@
-
-
+<div class="trending-game-container">
+  <button class="arrow left-arrow">&#8249;</button>
+  <div class="game-frame">
+    <img src="../media/covers/ds3.jpg" alt="Trending Game">
+    <div class="game-info">
+      <h2>Dark Souls III</h2>
+      <p>Dark Souls 3 is an action RPG set in a dark, interconnected world filled with challenging enemies, cryptic lore, and atmospheric locations. Players embark on a journey to rekindle the fading flame, facing relentless bosses and uncovering secrets. With precise combat mechanics and deep character customization, it’s a test of skill and perseverance.</p>
+      <span class="price">59,99€</span>
+      <div class="buttons">
+        <button class="buy-button">Acquista</button>
+        <button class="wishlist-button">Lista dei desideri</button>
+      </div>
+    </div>
+  </div>
+  <button class="arrow right-arrow">&#8250;</button>
+</div>
