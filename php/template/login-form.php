3babd14ce4d25d36f1d4bf0485172b5e83d6ccd4
alevu3344 <alessandro.valmori2@studio.unibo.it>
2025-01-11 18:26:05 +0100

    login form added

---
+++ php/template/login-form.php
@@ -0,0 +1,22 @@
+<section class="login-container">
+    <p class="title">Login</p>
+    <form class="form">
+        <fieldset>
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username" placeholder="">
+
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" placeholder="">
+
+            <p class="forgot">
+                <a rel="noopener noreferrer" href="#">Forgot Password?</a>
+            </p>
+
+            <button type="submit" class="sign">Sign in</button>
+        </fieldset>
+    </form>
+
+    <p class="signup">Don't have an account?
+        <a rel="noopener noreferrer" href="#" class="">Sign up</a>
+    </p>
+</section>
