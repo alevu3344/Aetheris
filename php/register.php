0e77dd0542225701e07b7ab2d4aeaac0099e9750
alevu3344 <alessandro.valmori2@studio.unibo.it>
2025-01-11 20:13:55 +0100

    added registration form

---
+++ php/register.php
@@ -0,0 +1,10 @@
+<?php
+require_once("bootstrap.php");
+
+$templateParams["titolo"] = "Blog TW - Register";
+
+$templateParams["nome"] = "registration-form.php";
+
+
+require("template/base.php");
+?>
---
+++ php/template/registration-form.php
@@ -0,0 +1,32 @@
+<section class="registration-container">
+    <p class="title">Register</p>
+    <form class="form">
+        <fieldset>
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name" placeholder="Your Name">
+
+            <label for="surname">Surname</label>
+            <input type="text" name="surname" id="surname" placeholder="Your Surname">
+
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" placeholder="Your Email">
+
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username" placeholder="Choose a Username">
+
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" placeholder="Choose a Password">
+
+            <label for="repeat-password">Repeat Password</label>
+            <input type="password" name="repeat-password" id="repeat-password" placeholder="Repeat your Password">
+
+            <p class="forgot">
+                <a rel="noopener noreferrer" href="#">Already have an account? Login</a>
+            </p>
+
+            <button type="submit" class="sign">Register</button>
+        </fieldset>
+    </form>
+
+    <p class="signup">By registering, you agree to our terms and conditions.</p>
+</section>
