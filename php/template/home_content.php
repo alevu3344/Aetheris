56677c1ea76ad61742ae6cd64bf97fba219f6915
Gabriel Stira <Gabriel.Stira@studio.unibo.it>
2025-01-17 18:57:39 +0100

    Giochi del momento

--- php/template/home_content.php
+++ php/template/home_content.php
@@ -1,20 +1,24 @@
+<article class="front-page-game">
+  <figure>
+    <img src="../media/covers/legend_of_zelda_todk.jpg" alt="Front page game">
+    <div class="front-page-game-overlay"></div>
+    <figcaption class="figcaption-front-page">
+      <p class="front-page-game-title">The Legend Of Zelda: Tears of The Kingdom</p>
+      <p class="description">Impedisci a Ganondorf di distruggere Hyrule.</p>
+    </figcaption>
 
-<div class="trending-game-container">
-  <button class="arrow left-arrow">&#8249;</button>
-  <div class="game-frame">
-    <img src="../media/covers/ds3.jpg" alt="Trending Game"/>
-    <div class="game-info">
-      <h2>Dark Souls III</h2>
-      <p>Dark Souls 3 is an action RPG set in a dark, interconnected world filled with challenging enemies, cryptic lore, and atmospheric locations. Players embark on a journey to rekindle the fading flame, facing relentless bosses and uncovering secrets. With precise combat mechanics and deep character customization, it’s a test of skill and perseverance.</p>
-      <span class="price">59,99€</span>
-      <div class="buttons">
-        <button class="buy-button">Acquista</button>
-        <button class="wishlist-button">Lista dei desideri</button>
+    <div class="front-page-game-buy-details">
+      <div class="price-and-buy-button">
+        <span class="front-page-game-price">29,99€</span>
+        <button class="front-page-game-buy-button">Acquista</button>
       </div>
+      <button class="front-page-game-add-to-cart">
+        <img src="upload/icons/add-to-cart.svg" alt="Add to cart icon"/>
+        Aggiungi al carrello
+      </button>
     </div>
-  </div>
-  <button class="arrow right-arrow">&#8250;</button>
-</div>
+  </figure>
+</article>
 
 
 
@@ -23,7 +27,11 @@
   <tr>
     <td rowspan="2">
       <article class="bigger-game">
-        <img src="../media/covers/rdr2.jpg" alt="Big Game">
+        <figure>
+          <img src="../media/covers/rdr2.jpg" alt="Big Game">
+          <figcaption class="figcaption-scroller">Red Dead Redemption 2</figcaption>
+        </figure>
+
           <section class="specifiche">
             <p class="offerta">OFFERTA A TEMPO LIMITATO</p>
             <p class="durata">Fino al 30 novembre!</p>
@@ -41,7 +49,10 @@
 
     <td>
       <article class="top-game">
-        <img src="../media/covers/uncharted4.jpg" alt="Top game"/>
+        <figure>
+          <img src="../media/covers/uncharted4.jpg" alt="Top game"/>
+          <figcaption class="figcaption-scroller">Uncharted 4</figcaption>
+        </figure>
           <span class="offerta-container">
           <span class="sconto">-25%</span>
           <span class="prezzi">
@@ -56,7 +67,11 @@
   <tr>
     <td>
       <article class="bottom-game">
+        <figure>
         <img src="../media/covers/ds2.jpg" alt="Bottom game"/>
+          <figcaption class="figcaption-scroller">Dark Souls 2</figcaption>
+        </figure>
+        
           <span class="offerta-container">
           <span class="sconto">-75%</span>
           <span class="prezzi">
