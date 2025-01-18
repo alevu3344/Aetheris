f333df8299b37d9b0b617b79bc51397877a9d688
Gabriel Stira <Gabriel.Stira@studio.unibo.it>
2025-01-18 19:12:10 +0100

    Table now properly working

--- php/template/home_content.php
+++ php/template/home_content.php
@@ -35,59 +35,52 @@
 <table>
   <tr>
     <td rowspan="2">
-      <article class="bigger-game">
+      <article>
         <figure>
           <img src="../media/covers/5.jpg" alt="Big Game">
-          <figcaption class="figcaption-scroller">Assassin's Creed Valhalla</figcaption>
+          <figcaption>Assassin's Creed Valhalla</figcaption>
         </figure>
 
-          <section class="specifiche">
-            <p class="offerta">OFFERTA A TEMPO LIMITATO</p>
-            <p class="durata">Fino al 30 novembre!</p>
+        <section>
+          <p>OFFERTA A TEMPO LIMITATO</p>
+          <p>Fino al 30 novembre!</p>
 
-            <span class="offerta-container">
-              <span class="sconto">-50%</span>
-              <span class="prezzi">
-                <span class="prezzo-originale">29,99€</span>
-                <span class="prezzo-scontato">14,99€</span>
-              </span>
-            </span>
+          <footer>
+            <span>-50%</span>
+            <span>29,99€</span>
+            <span>14,99€</span>
+          </footer>
         </section>
       </article>
     </td>
 
     <td>
-      <article class="top-game">
+      <article>
         <figure>
           <img src="../media/covers/23.jpg" alt="Top game"/>
-          <figcaption class="figcaption-scroller">Cyberpunk 2077</figcaption>
+          <figcaption>Cyberpunk 2077</figcaption>
         </figure>
-          <span class="offerta-container">
-          <span class="sconto">-25%</span>
-          <span class="prezzi">
-            <span class="prezzo-originale">59,99€</span>
-            <span class="prezzo-scontato">44,99€</span>
-          </span>
-        </span>
+        <footer>
+          <span>-25%</span>
+          <span>59,99€</span>
+          <span>44,99€</span>
+        </footer>
       </article>
     </td>
 
   </tr>
   <tr>
     <td>
-      <article class="bottom-game">
+      <article>
         <figure>
         <img src="../media/covers/44.jpg" alt="Bottom game"/>
-          <figcaption class="figcaption-scroller">Dead Cells</figcaption>
+          <figcaption>Dead Cells</figcaption>
         </figure>
-        
-          <span class="offerta-container">
-          <span class="sconto">-75%</span>
-          <span class="prezzi">
-            <span class="prezzo-originale">19,99€</span>
-            <span class="prezzo-scontato">4,99€</span>
-          </span>
-        </span>
+        <footer>
+          <span>-75%</span>
+          <span>19,99€</span>
+          <span>4,99€</span>
+        </footer>
       </article>
     </td>
   </tr> 
