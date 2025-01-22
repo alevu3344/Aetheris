6357388a113454551c8b403c3b122d5db552be2f
alevu3344 <alessandro.valmori2@studio.unibo.it>
2025-01-22 14:43:21 +0100

    actual WORKING LOGIN (DOM gets modified)

--- js/login.js
+++ js/login.js
@@ -3,72 +3,29 @@ function generateLoginForm(loginerror = null) {
     document.querySelector("body").className = "login-form";
     let form = `
     <section>
-        <div>
-            <img src="upload/icons/cross.png" alt="Logo">
-        </div>
-        <h2>Login</h2>
-        <form>
-            <fieldset>
-                <label for="username">Username</label>
-                <input type="text" name="username" id="username" placeholder="">
+    <div>
+        <img src="upload/icons/cross.png" alt="Logo">
+    </div>
+    <h2>Login</h2>
+    <form>
+        <fieldset>
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username" placeholder="">
 
-                <label for="password">Password</label>
-                <input type="password" name="password" id="password" placeholder="">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" placeholder="">
 
-                <p>
-                    <a rel="noopener noreferrer" href="#">Forgot Password?</a>
-                </p>
+            <p>
+                <a rel="noopener noreferrer" href="#">Forgot Password?</a>
+            </p>
 
-                <button type="submit">Sign in</button>
-            </fieldset>
-        </form>
+            <button type="submit">Sign in</button>
+        </fieldset>
+    </form>
 
-        <p>Don't have an account?
-            <a>Sign up</a>
-        </p>
-    </section>
-    `;
-    return form;
-}
-
-function generateRegistrationForm(loginerror = null) {
-    //modify the class of the body element in the DOM
-    document.querySelector("body").className = "registration-form";
-    let form = `
-    <section>
-        <div>
-            <img src="upload/icons/cross.png" alt="Logo">
-        </div>
-        <h2>Register</h2>
-        <form>
-            <fieldset>
-                <label for="name">Name</label>
-                <input type="text" name="name" id="name" placeholder="Your Name">
-
-                <label for="surname">Surname</label>
-                <input type="text" name="surname" id="surname" placeholder="Your Surname">
-
-                <label for="email">Email</label>
-                <input type="email" name="email" id="email" placeholder="Your Email">
-
-                <label for="username">Username</label>
-                <input type="text" name="username" id="username" placeholder="Choose a Username">
-
-                <label for="password">Password</label>
-                <input type="password" name="password" id="password" placeholder="Choose a Password">
-
-                <label for="repeat-password">Repeat Password</label>
-                <input type="password" name="repeat-password" id="repeat-password" placeholder="Repeat your Password">
-
-                <p class>
-                    <a id="back-to-login" href="#">Already have an account? Login</a>
-                </p>
-
-                <button type="submit">Register</button>
-            </fieldset>
-        </form>
-
-        <p>By registering, you agree to our terms and conditions.</p>
+    <p>Don't have an account?
+        <a rel="noopener noreferrer" href="#" class="">Sign up</a>
+    </p>
 </section>
     `;
     return form;
@@ -130,55 +87,27 @@ getLoginData();
 
 
 function createLoginForm() {
+
     const previousContent = main.innerHTML;
     const previousBodyClass = document.querySelector("body").className;
-
-    // Generate the login form
+    // Utente NON loggato
     let form = generateLoginForm();
     main.innerHTML = form;
 
-    // Event listener for switching to the registration form
-    document.querySelector(".login-form > section > p:last-child > a").addEventListener("click", function () {
-        createRegistrationForm(previousContent, previousBodyClass);
-    });
-
     // Handle the close button click
-    document.querySelector(".login-form > section > div > img").addEventListener("click", function () {
-        document.querySelector("body").className = previousBodyClass; // Restore previous body class
-        main.innerHTML = previousContent; // Restore previous content
-        console.log("Close button clicked");
+    document.querySelector(".login-form > main > section > div > img").addEventListener("click", function () {
+        document.querySelector("body").className = previousBodyClass; // Restore
+        main.innerHTML = previousContent; // Restore the previous content
     });
-
-    // Handle login form submission
-    document.querySelector(".login-form > section > form").addEventListener("submit", function (event) {
+    // Gestisco tentativo di login
+    document.querySelector(".login-form > main > section > form").addEventListener("submit", function (event) {
         event.preventDefault();
+        console.log("Login form submitted");
         const username = document.querySelector("#username").value;
         const password = document.querySelector("#password").value;
         login(username, password);
     });
 }
-
-function createRegistrationForm(previousContent, previousBodyClass) {
-    // Generate the registration form
-    let form = generateRegistrationForm();
-    main.innerHTML = form;
-    document.querySelector("body").className = "registration-form";
-
-    // Add event listener for "Back to Login" in the registration form
-    document.querySelector("#back-to-login").addEventListener("click", function (event) {
-        event.preventDefault();
-        createLoginForm(previousContent, previousBodyClass); // Restore the previous state
-    });
-
-    // Handle the close button click
-    document.querySelector(".registration-form > section > div > img").addEventListener("click", function () {
-        document.querySelector("body").className = previousBodyClass; // Restore previous body class
-        main.innerHTML = previousContent; // Restore previous content
-    });
-}
-
-
-
 
 async function login(username, password) {
     const url = 'login-api.php';
