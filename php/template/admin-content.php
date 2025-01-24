5f8637085da10f2fdc21e51d72cf6cd3a1d21423
alevu3344 <alessandro.valmori2@studio.unibo.it>
2025-01-24 14:18:33 +0100

    made the blue print for an admin panel (static)

--- php/template/admin-content.php
+++ php/template/admin-content.php
@@ -5,30 +5,44 @@
             <img src="../media/covers/<?=$gioco["Id"]?>.jpg"/>
             <div>
                 <dl>
-                    <dt>Titolo: </dt>
-                    <dd><?= $gioco["Name"]?></dd>
-                    <dt>Generi</dt>
-                    <dd>
-                        <?php foreach($gioco["Categories"] as $categoria): ?>              
-                            <span><?=$categoria["CategoryName"]?></span>
-                        <?php endforeach; ?>
-                 
-                    </dd>
-                    <dt>Piattaforme</dt>
-                    <dd>
-                        <?php foreach($gioco["Platforms"] as $platform): ?>
-                            <img src="upload/icons/<?= $platform["Platform"]?>.svg"/>
-                        <?php endforeach; ?>
-                    </dd>
-                    <dt>Sviluppatore</dt>
-                    <dd><?= $gioco["Publisher"]?></dd>
+                    <div>
+                        <dt>Titolo: </dt>
+                        <dd><?= $gioco["Name"]?></dd>
+                    </div>
+                    <div id="categories">
+                        <dt>Generi: </dt>
+                        <dd>
+                            <?php foreach($gioco["Categories"] as $categoria): ?>              
+                                <span><?=$categoria["CategoryName"]?></span>
+                            <?php endforeach; ?>
+                    
+                        </dd>
+                    </div>
+                    <div>
+                        <dt>Piattaforme: </dt>
+                        <dd>
+                            <?php foreach($gioco["Platforms"] as $platform): ?>
+                                <img src="upload/icons/<?= $platform["Platform"]?>.svg"/>
+                            <?php endforeach; ?>
+                        </dd>
+                    </div>
+                    <div>
+                        <dt>Sviluppatore: </dt>
+                        <dd><?= $gioco["Publisher"]?></dd>
+                    </div>
                     <dl>
-                        <dt>Sconto</dt>
-                        <dd>-40%</dd>
-                        <dt>Inizio</dt>
-                        <dd>01/01/2021</dd>
-                        <dt>Fine</dt>
-                        <dd>01/01/2022</dd>
+                        <div>
+                            <dt>Sconto: </dt>
+                            <dd>-40%</dd>
+                        </div>
+                        <div>
+                            <dt>Inizio: </dt>
+                            <dd>01/01/2021</dd>
+                        </div>
+                        <div>
+                            <dt>Fine: </dt>
+                            <dd>01/01/2022</dd>
+                        </div>
                     </dl>
                 </dl>
             </div>
