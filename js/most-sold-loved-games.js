8ac0f6b063410778fe99aa76d81f2d52fbfedfbd
Gabriel Stira <Gabriel.Stira@studio.unibo.it>
2025-01-25 16:28:18 +0100

    Working

--- js/most-sold-loved-games.js
+++ js/most-sold-loved-games.js
@@ -1,19 +1,26 @@
-document.querySelector(".home_content > main > nav > ul > li:first-child > h2").addEventListener('click', function() {
+const piuAcquistati = document.querySelector(".home_content > main > nav > ul > li:first-child > h2");
+const piuAmati = document.querySelector(".home_content > main > nav > ul > li:last-child > h2");
+
+piuAcquistati.addEventListener('click', function() {
     setTimeout(() => {
         lovedGames.classList.remove("crumbleInverse");
         lovedGames.classList.add("crumble");
+        piuAmati.classList.remove("sbrilluccichio");
 
+        piuAcquistati.classList.add("sbrilluccichio");
         soldGames.classList.remove("crumble");
         soldGames.classList.add("crumbleInverse");
     }, 500);
 });
 
-document.querySelector(".home_content > main > nav > ul > li:last-child > h2").addEventListener('click', function() {
+piuAmati.addEventListener('click', function() {
     setTimeout(() => {
+        piuAcquistati.classList.remove("sbrilluccichio");
         lovedGames.classList.remove("notVisible");
         soldGames.classList.remove("crumbleInverse");
         soldGames.classList.add("crumble");
 
+        piuAmati.classList.add("sbrilluccichio");
         lovedGames.classList.remove("crumble");
         lovedGames.classList.add("crumbleInverse");
     }, 500);
@@ -23,3 +30,4 @@ const soldGames = document.querySelector(".home_content >main>div:nth-child(5)> 
 const lovedGames = document.querySelector(".home_content >main>div:nth-child(5)> div:last-child");
 
 lovedGames.classList.add("notVisible");
+piuAcquistati.classList.add("sbrilluccichio");
