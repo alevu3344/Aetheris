fddfea8a2dc0d60f0dc7fda05c255261b129f2b3
alevu3344 <alessandro.valmori2@studio.unibo.it>
2025-02-05 21:12:30 +0100

    missing mockup : mobile(admin-panel)

--- js/base.js
+++ js/base.js
@@ -1,18 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const categoriesToggle = document.getElementById("categories-toggle");
 
-    // Check if the categories-toggle element is present in the DOM
-    if (categoriesToggle) {
-        categoriesToggle.addEventListener("click", function() {
-            this.classList.toggle("active");
-            const categoriesList = document.getElementById("categories-list");
-            categoriesList.style.display = categoriesList.style.display === "none" ? "block" : "none";
-        });
-    }
-});
+const categoriesToggle = document.getElementById("categories-toggle");
+
+// Check if the categories-toggle element is present in the DOM
+if (categoriesToggle) {
+    categoriesToggle.addEventListener("click", function () {
+        this.classList.toggle("active");
+        const categoriesList = document.getElementById("categories-list");
+        categoriesList.style.display = categoriesList.style.display === "none" ? "block" : "none";
+    });
+}
+
 
 const categoriesList = document.getElementById("categories-list");
-if(categoriesList!=null) {
+if (categoriesList != null) {
     categoriesList.style.display = "none";
 }
 
